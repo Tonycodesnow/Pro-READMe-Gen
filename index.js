66760b1edde614f2539e03f2ff81355c74c86ba0
Ant <huertinez.family@outlook.com>
2021-09-25 00:24:15 -0700

    added questions to readme

--- index.js
+++ index.js
@@ -3,13 +3,65 @@ const inquirer = require ('inquirer');
 
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [
+
+const promptUser = () => {
+    return inquirer.prompt([
     {
         type: "input",
         name: "title",
         message: "what is the title of your project",
+    },
+    {
+        type: "input",
+        message: "Enter a description for your project:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Enter your GitHub Username: (Required)",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "Enter your email address:",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Enter your project instructions for use:",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Enter the step by step installation instructions:",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "Enter contribution guidelines for your project:",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "Enter any test information for your project:",
+        name: "testing"
+    },
+    {
+        type: "list",
+        message: "Which license does this project fall under?",
+        name: "license",
+        choices: [
+            "Apache",
+            "Code Project Open(CPOL)",
+            "Eclipse",
+            "GNU",
+            "MIT ",
+            "Mozilla(MPL 1.1)",
+        ]
     }
-];
+    ]);
+};
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
